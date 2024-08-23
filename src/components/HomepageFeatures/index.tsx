f5b3e115ecf033f3aa377caacfe8cdac49f32ba5
neemajd20 <neemajd20@gmail.com>
2024-08-23 14:03:03 +0300

    fix:add:src/css/custom.css,src/components/HomepageFeatures/index.tsx

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -9,37 +9,17 @@ type FeatureItem = {
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/Logo.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
+ 
   {
     title: 'Ministry of Health',
     Svg: require('@site/static/img/Logo.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
+      Afyamsafiri is a user guide for electronic screening of travelers and public health events at a point of entry. 
+    </>
     ),
   }
-  ,
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/Logo.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+  
 ];
 
 function Feature({title, Svg, description}: FeatureItem) {
